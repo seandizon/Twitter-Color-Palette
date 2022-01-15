96d01909c8eb8cc9258738749015ec4ecfbedb2d
seandizon <43098042+seandizon@users.noreply.github.com>
2022-01-14 19:55:39 -0800

    Split image processing into functions.

--- art_api.py
+++ art_api.py
@@ -10,35 +10,47 @@ import matplotlib.pyplot as plt
 art_url = "https://api.artic.edu/api/v1/artworks"
 pp = pprint.PrettyPrinter(indent=4)
 
-with Image.open("3.jpg") as im:
-    img = numpy.array(im)
+# This function will eventually change to get an image using the AIC api.
+# Current version is for testing purposes.
+def get_image(img_name: str) -> numpy.ndarray:
+    with Image.open(img_name) as im:
+        return numpy.array(im)
 
-# turn 3d array to 2d array
-# -1 bc we don't know how many rows we'll get (technically we do, it's original row * column)
-# 3 for 3 columns: r,g,b
-new_img = img.reshape((-1,3))
+# Matplotlibs requires ints (1-255) or floats (0.00-1.00).
+# Since centers are floats between 1-255, divide by 255 to maintain accuracy.
+def convert_to_float_points(clusters: numpy.ndarray, n: int) -> None:
+    for i in range(n):
+        for j in range(3):
+            clusters[i][j] /= 255
 
-km = KMeans(n_clusters=10).fit(new_img)
+# Show dominant colors on a plot.
+def plot_colors(clusters: numpy.ndarray, n: int) -> None:
+    # Need to turn the cluster centers from 2d to 3d.
+    kc = []
+    for i in range(n):
+        kc.append([[x for x in clusters[i]]])
 
-for i in range(10):
-    for j in range(3):
-        # matplotlibs requires ints(1-255) or floats(0-1)
-        # since centers are floats between 1-22, divide by 255 to maintain accuracy
-        km.cluster_centers_[i][j] /= 255
+    # Show palette on a graph.
+    plt.imshow(kc)
+    plt.show()
 
+# Will eventually return an image.
+def get_palette(n: int) -> None:
+    # Get 3d array from image and turn into a 2d array for k-means.
+    # -1 bc we don't know how many rows we'll get (technically we do, it's original row * column).
+    # 3 for 3 columns: r,g,b.
+    img = get_image("5.jpg").reshape((-1,3))
 
-# Need to turn the cluster centers from 2d to 3d
-kc = []
-for i in range(len(km.cluster_centers_)):
-    kc.append([[x for x in km.cluster_centers_[i]]])
+    # K-means clustering finds n dominant colors.
+    km = KMeans(n_clusters=n).fit(img).cluster_centers_
 
-# Show palette on a graph
-plt.imshow(kc)
-plt.show()
+    # Convert centers to points for plotting.
+    convert_to_float_points(km, n)
 
-# Show color
-# plt.imshow([[tuple(img[540][1140])]])
-# plt.show()
+    # Plot.
+    plot_colors(km, n)
+
+get_palette(10)
 
 # Example way for api get requests
 # payload = {
