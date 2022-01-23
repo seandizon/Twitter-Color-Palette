224f37ab0ca3e5d949dbb519eae1de26b46ec554
seandizon <43098042+seandizon@users.noreply.github.com>
2022-01-22 17:31:10 -0800

    Twitter API setup
    Connected to the twitter api v1.1.

--- main.py
+++ main.py
@@ -2,6 +2,7 @@
 
 import art_api as ap
 import clustering as cl
+import twitter_api as tw
 
 import pprint
 
@@ -9,16 +10,20 @@ pp = pprint.PrettyPrinter(indent=4)
 
 def main():
     # Get a random image along with any required information about it.
-    img_info = ap.get_random_art()[0]
-    pp.pprint(img_info)
+    # img_info = ap.get_random_art()[0]
+    # pp.pprint(img_info)
+    #
+    # # Perform k-means and draw.
+    # NUM_CLUSTERS = 10
+    # img_url = ap.create_image_link(img_info["image_id"])
+    # print(img_url)
+    # cl.get_palette(img_url, NUM_CLUSTERS)
 
-    # Perform k-means.
-    NUM_CLUSTERS = 10
-    img_url = ap.create_image_link(img_info["image_id"])
-    print(img_url)
-    cl.get_palette(img_url, NUM_CLUSTERS)
-    # Draw palette.
     # Upload to twitter.
+    api = tw.get_api()
+    api.update_status(status="Test with 1.1")
+
+
 
 if __name__ == "__main__":
     main()
--- twitter_api.py
+++ twitter_api.py
@@ -1,2 +1,19 @@
 # Twitter authentication and uploading of tweets including artwork and palette
+import os
 import tweepy
+from dotenv import load_dotenv
+
+# Comment out in production
+load_dotenv()
+
+def get_api() -> tweepy.API:
+    consumer_key = os.environ.get("CONSUMER_KEY")
+    consumer_secret = os.environ.get("CONSUMER_SECRET")
+    access_token = os.environ.get("ACCESS_TOKEN")
+    access_token_secret = os.environ.get("ACCESS_SECRET")
+
+    auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+    auth.set_access_token(access_token, access_token_secret)
+
+    return tweepy.API(auth)
+
